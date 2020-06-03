bc22ae8f72c60c4ef683d6ff855142a8ad614513
IdlePhysicist <idlephysicist@vivaldi.net>
2020-06-03 22:36:56 +0100

    Adding version variable for HEAD build

--- cave-logger.rb
+++ cave-logger.rb
@@ -3,7 +3,8 @@ class CaveLogger < Formula
   desc "A caving database interface written in Go."
   revision 1
 
-  url "https://github.com/IdlePhysicist/cave-logger/archive/v1.2.tar.gz"
+  version "1.2"
+  url "https://github.com/IdlePhysicist/cave-logger/archive/v#{version}.tar.gz"
   sha256 "ce3b66a3ac59d92bef1319c716a609884b2fdbeaa2f56214d4e8026344bdad1d"
 
   head "https://github.com/IdlePhysicist/cave-logger.git"
@@ -11,12 +12,16 @@ class CaveLogger < Formula
   depends_on "go" => :build
   depends_on "python3" => :build
 
+  if build.head?
+    version "HEAD"
+  end
+
   def install
     bin.mkpath
 
     # ENV.deparallelize  # if your formula fails when building in parallel
     system "python3", "./scripts/make-db.py"
-    system "make", "BUILD=#{bin}"
+    system "make", "BUILD=#{bin}", "version=#{version}"
   end
 
   test do
