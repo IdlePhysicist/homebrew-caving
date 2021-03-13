004f3a0e71120984d8dcdfe57e274e37b307a857
IdlePhysicist <idlephysicist@vivaldi.net>
2021-03-13 23:29:48 +0000

    Updated cave logger formula to v2.0.0-rc1

--- cave-logger.rb
+++ cave-logger.rb
@@ -3,28 +3,25 @@ class CaveLogger < Formula
   desc "A caving database interface written in Go."
   revision 1
 
-  version "2.0.0-b1"
-  url "https://github.com/IdlePhysicist/cave-logger/archive/v#{version}.tar.gz"
-  sha256 "76d102af51adaca2735825df7deafff1aa16c9d658a2845d27136b2ebcdb151e"
+  version "v2.0.0-rc1"
+  url "https://github.com/IdlePhysicist/cave-logger/archive/#{version}.tar.gz"
+  sha256 "5b95e03dcc44ef91b8fbcc9da6c8c74610dd639ce1a9a2e41086c02972193913"
 
   head "https://github.com/IdlePhysicist/cave-logger.git"
 
   depends_on "go" => :build
   depends_on "python3" => :build
 
-  if build.head?
-    version "HEAD"
-  end
 
   def install
     bin.mkpath
 
     # ENV.deparallelize  # if your formula fails when building in parallel
     system "python3", "./scripts/make-db.py"
-    if OS.mac?
-      system "make", "darwin", "BUILD=#{bin}", "version=#{version}"
+    if build.head?
+      system "make", "BUILD=#{bin}"
     else
-      system "make", "linux", "BUILD=#{bin}", "version=#{version}"
+      system "make", "BUILD=#{bin}", "version=#{version}"
     end
 
   end
